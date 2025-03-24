dfd97b7f7b6790fddfd723608b5ba22c9420cf54
Sarah <riallandtardy@et.esiea.fr>
2025-03-24 20:10:19 +0100

    premières requêtes mongodb

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
 import streamlit as st
-from queries.mongo_queries import *
-from database.mongo import get_database
-
-db = get_database()  # Récupération de la base de données
-fdb = db["films"]
-
-st.title("NoSQL Databases - Projet")
-st.header("Requêtes MongoDB")
-
-print(fdb.count_documents())
-#st.write("Année avec le plus grand nombre de films sortis :", q1(fdb)['_id'], "avec", q1(fdb)['count'], "films.")
-#st.write("Nombre de films sortis après 1999 :", q2(fdb))
-
+st.title("NoSQL")
+st.header("film")
