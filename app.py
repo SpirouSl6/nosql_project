9d3a6a963bd65ca8fe73f56fc77c163f278731d2
Sarah <riallandtardy@et.esiea.fr>
2025-03-24 20:51:39 +0100

    premières requêtes mongodb

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ st.title("NoSQL Databases - Projet")
 st.header("Requêtes MongoDB")
 
 st.write("Nombre total de documents :", db.films.count_documents({}))
-st.write("Année avec le plus grand nombre de films sortis : {q1(db)[0]['_id']} avec {q1(db)[0]['count']} films.")
+st.write("Année avec le plus grand nombre de films sortis : ", q1(db)[0]['_id'],  "avec", q1(db)[0]['count'], "films.")
 #st.write("Nombre de films sortis après 1999 : {q2(db)}")
