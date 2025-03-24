a53bf3099dccbe8af6e0675bed5edcd2c16f51fd
Sarah <riallandtardy@et.esiea.fr>
2025-03-24 21:07:18 +0100

    3-4 requêtes mongodb

--- app.py
+++ app.py
@@ -8,5 +8,17 @@ st.title("NoSQL Databases - Projet")
 st.header("Requêtes MongoDB")
 
 st.write("Nombre total de documents :", db.films.count_documents({}))
-st.write("Année avec le plus grand nombre de films sortis : ", q1(db)[0]['_id'],  "avec", q1(db)[0]['count'], "films.")
+st.write("Année avec le plus grand nombre de films sortis : ", q1(db)['_id'],  "avec", q1(db)['count'], "films.")
 st.write("Nombre de films sortis après 1999 : ", q2(db))
+st.write("Moyenne des votes des films sortis en 2007. : ", q3(db))
+st.write("Histogramme nombres de films par année : ", q4(db))
+
+
+
+
+
+
+
+
+
+
--- queries/mongo_queries.py
+++ queries/mongo_queries.py
@@ -1,5 +1,7 @@
-from queries.mongo_queries import *
-from database.mongo import get_database
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import scipy.stats as stats
 
 def q1(db):
     pipeline = [
@@ -7,9 +9,32 @@ def q1(db):
         {"$sort": {"count": -1}},
         {"$limit": 1}]
     result = db.films.aggregate(pipeline)
-    return list(result)
+    return list(result)[0]
 
 def q2(db):
     result = db.films.count_documents({"year": {"$gt": 1999}})
     return result
+
+
+def q3(db):
+    pipeline = [
+        {"$match": {"year": 2007}},
+        {"$group": {"_id": None, "avg_rating": {"$avg": "$rating"}}}]
+    result = list(db.films.aggregate(pipeline))
+    return result[0]["avg_rating"]
+
+
+def q4(db):
+    pipeline = [
+        {"$group": {"_id": "$year", "count": {"$sum": 1}}},
+        {"$sort": {"_id": 1}}]
+    data = list(db.films.aggregate(pipeline))
+    years = [d["_id"] for d in data]    # Liste des années
+    counts = [d["count"] for d in data]   # Liste des nombres de films
+
+    sns.barplot(x=years, y=counts)  #Crée un histogramme
+    plt.xlabel("Année")
+    plt.ylabel("Nombre de films")
+    plt.title("Nombre de films par année")
+    plt.show()
 
