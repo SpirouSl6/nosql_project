3d2dd95aa085164cc2fc7fada0c3d74115c59c14
Sarah <riallandtardy@et.esiea.fr>
2025-03-24 16:09:52 +0100

    test streamlit

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import streamlit as st
-from database.mongo import get_database
 
-
-st.title("Exploration de bases de données NoSQL")
-
-db = get_database()
-
-st.header("Année avec le plus grand nombre de films")
+st.title("NoSQL")
+st.header("film")
 
