75f484837a925ac10ffdd4a5295f105392b14450
Sarah <riallandtardy@et.esiea.fr>
2025-03-24 20:02:06 +0100

    premières requêtes mongodb

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from queries.mongo_queries import *
 from database.mongo import get_database
 
 db = get_database()  # Récupération de la base de données
+fdb = db["films"]
 
 st.title("NoSQL Databases - Projet")
 st.header("Requêtes MongoDB")
 
-result_q1 = q1(db)
-st.write("Année avec le plus grand nombre de films sortis :", result_q1[0]['_id'], "avec", result_q1[0]['count'], "films.")
-st.write("Nombre de films sortis après 1999 :", q2(db))
+print(fdb.count_documents())
+#st.write("Année avec le plus grand nombre de films sortis :", q1(fdb)['_id'], "avec", q1(fdb)['count'], "films.")
+#st.write("Nombre de films sortis après 1999 :", q2(fdb))
 
--- database/mongo.py
+++ database/mongo.py
@@ -7,3 +7,6 @@ def get_mongo_client():
 def get_database():
     client = get_mongo_client()
     return client["entertainment"]
+
+if __name__ == "__main__":   
+   dbname = get_database()
