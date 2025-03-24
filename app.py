60a452f2275a324393625bbd64204d6c54ee93d4
SpirouSl6 <sarah.rialland_tardy@hotmail.com>
2025-03-24 18:05:46 +0100

    premières requêtes mongodb

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from queries.mongo_queries import *
 st.title("NoSQL Databases - Projet")
 st.header("Requêtes MongoDB")
 
-st.write("Année avec le plus grand nombre de films sortis :", q1()["year"])
+st.write("Année avec le plus grand nombre de films sortis :", q1()[0]['_id'], "avec", q1()[0]['count'], "films.")
 st.write("Nombre de films sortis après 1999 :", q2())
 
