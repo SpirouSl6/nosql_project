0f86295cde4e4754969b9654a6b9a1ea54535308
Sarah <riallandtardy@et.esiea.fr>
2025-03-24 17:59:53 +0100

    premières requêtes mongodb

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+from queries.mongo_queries import *
 
-st.title("NoSQL")
-st.header("film")
+st.title("NoSQL Databases - Projet")
+st.header("Requêtes MongoDB")
+
+st.write("Année avec le plus grand nombre de films sortis :", q1()["year"])
+st.write("Nombre de films sortis après 1999 :", q2())
 
---
+++ queries/mongo_queries.py
@@ -0,0 +1,14 @@
+def q1(db):
+    pipeline = [
+        {"$group": {"_id": "$year", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 1}]
+    result = db.films.aggregate(pipeline)
+    return list(result)
+
+
+def q2(db):
+    pipeline = [{"year": {"$gt": 1999}}]
+    result = db.films.count_documents(pipeline)
+    return result
+
