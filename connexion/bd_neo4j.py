f2c63ab2518fee842c8e27e691afd8c627c7b13a
Sarah <riallandtardy@et.esiea.fr>
2025-03-28 00:33:01 +0100

    Requetes neo4j + affichage

--- connexion/bd_neo4j.py
+++ connexion/bd_neo4j.py
@@ -14,13 +14,14 @@ def noeud_film(film):
     query = """
     MERGE (f:Films {id: $id})
     SET f.title = $title, f.year = $year, f.votes = $votes, 
-        f.revenue = $revenue, f.rating = $rating, f.director = $director
+        f.revenue = $revenue, f.rating = $rating, f.director = $director, f.genre = $genre
     """
     # Passer les paramètres dans un dictionnaire
     parameters = {
         "id": film["_id"],
         "title": film["title"],
         "year": film["year"],
+        "genre": film["genre"],
         "votes": film.get("Votes", 0),
         "revenue": film.get("Revenue (Millions)", 0),
         "rating": film.get("rating", "unrated"),
@@ -46,7 +47,30 @@ def relation_a_joue(actor_name, film_id):
     """
     parameters = {"name": actor_name.strip(), "id": film_id}
     conn.query(query, parameters)
-      
+
+
+# Fonction pour insérer les relations "A réaliser"
+def relation_a_realiser(director_name, film_id):
+    query = """
+    MATCH (f:Films {id: $id})
+    MERGE (d:Realisateur {name: $director_name})
+    MERGE (d)-[:A_REALISE]->(f);
+    """
+    parameters = {"id": film_id, "director_name": director_name.strip()}
+    conn.query(query, parameters)
+    
+
+# Fonction pour insérer les relations "A réaliser"
+def relation_influence_par():
+    query = """
+    MATCH (d1:Realisateur)-[:A_REALISE]->(f1:Films)
+    MATCH (d2:Realisateur)-[:A_REALISE]->(f2:Films)
+    WHERE d1 <> d2 
+    AND ANY(genre IN f1.genre WHERE genre IN f2.genre)
+    MERGE (d1)-[:INFLUENCE_PAR]->(d2);
+    """
+    conn.query(query)
+          
             
 # Création du nœud acteur et relation avec le film
 def add_actor_me():
@@ -70,17 +94,18 @@ def importation():
     # Insérer les films depuis MongoDB vers Neo4j
     for film in db.films.find({}, {"_id": 1, "title": 1, "year": 1, 
                                 "Votes": 1, "Revenue (Millions)": 1, 
-                                "rating": 1, "Director": 1}):
+                                "rating": 1, "Director": 1, "genre": 1}):
         noeud_film(film)
     print("Importation des films terminée !")
     
     # Insérer les films depuis MongoDB vers Neo4j
     for actor in db.films.aggregate([
-            {"$project": {"_id": 1, "title": 1, "year": 1, "Votes": 1, "Revenue (Millions)": 1, "rating": 1, "Director": 1,"Actors": { "$split": ["$Actors", ", "]}}}, 
+            {"$project": {"_id": 1, "title": 1, "year": 1, "Votes": 1, "Revenue (Millions)": 1, "rating": 1, "Director": 1, "genre": 1, "Actors": { "$split": ["$Actors", ", "]}}}, 
             {"$unwind": "$Actors"}, 
             {"$set": {"Actors": { "$trim": { "input": "$Actors"}}}}]):
         actor_name = actor["Actors"]
         film_id = actor["_id"]
+        director_name = actor["Director"]
 
         noeud_actor(actor_name)  # Création du nœud acteur
         relation_a_joue(actor_name, film_id)  # Création de la relation
@@ -94,6 +119,14 @@ def importation():
         if director.get("Director"):
             noeud_director(director["Director"])
     print("Importation des directors terminée !")
+    
+    for film in db.films.find({}, {"_id": 1, "title": 1, "Director": 1}):
+        if "Director" in film and film["Director"]:
+            relation_a_realiser(film["Director"], film["_id"])
+    print("Création des relations 'A_REALISE' terminée !")
+    
+    relation_influence_par()
+    print("Création des relations 'INFLUENCE_PAR' terminée !")
 
 
 importation()
--- connexion/neo4j_queries.py
+++ connexion/neo4j_queries.py
@@ -1,14 +1,108 @@
 from neo4j_co import Neo4jConnection
+import networkx as nx
+import community as community_louvain
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Récupérer et afficher les films dans Neo4j
 conn = Neo4jConnection()
 
-# Exécuter la requête pour récupérer les films
-films = conn.query("MATCH (f:Film) RETURN f LIMIT 10")  # Récupérer 10 films
+def q14():
+    result = conn.query("""MATCH (a:Actors)-[:A_JOUE]->(f:Films) 
+                        RETURN a.name, COUNT(f) AS FilmCount 
+                        ORDER BY FilmCount DESC 
+                        LIMIT 1""") 
+    return result
 
-# Afficher les résultats
-for film in films:
-    print(film)
+def q15():
+    result = conn.query(""""MATCH (a:Actors)-[:A_JOUE]->(f:Films)<-[:A_JOUE]-(ah:Actors {name: 'Anne Hathaway'}) 
+                        RETURN a.name""") 
+    return result
+
+def q16_1():
+    result = conn.query("""MATCH (a:Actors)-[:A_JOUE]->(f:Films) 
+                        RETURN a.name, f.revenue AS TotalRevenue 
+                        ORDER BY TotalRevenue DESC 
+                        LIMIT 1""") 
+    return result
+
+def q16_2():
+    result = conn.query("""MATCH (a:Actors)-[:A_JOUE]->(f:Films) 
+                        WITH a, SUM(COALESCE(toFloat(f.revenue), 0)) AS TotalRevenue 
+                        RETURN a.name, TotalRevenue 
+                        ORDER BY TotalRevenue DESC 
+                        LIMIT 1""") 
+    return result
+
+def q17():
+    result = conn.query("""MATCH (f:Films) 
+                        RETURN AVG(f.votes)""") 
+    return result
+
+def q18():
+    result = conn.query("""MATCH (f:Films) 
+                        UNWIND split(f.genre, ',') AS genre 
+                        RETURN genre, COUNT(*) AS genre_count 
+                        ORDER BY genre_count DESC 
+                        LIMIT 1""") 
+    return result
+
+def q19():
+    result = conn.query("""MATCH (a:Actors {name: 'Sarah Rialland Tardy'})-[:A_JOUE]->(f:Films)<-[:A_JOUE]-(other:Actors)-[:A_JOUE]->(fo:Films) 
+                        WHERE other.name <> a 
+                        RETURN fo.title""") 
+    return result
+
+def q20():
+    result = conn.query("""MATCH (d:Realisateur)-[:A_REALISE]->(f:Films)<-[:A_JOUE]-(a:Actors)
+                        RETURN d.name, COUNT(DISTINCT a) AS NombreActeurs
+                        ORDER BY NombreActeurs DESC
+                        LIMIT 1;""") 
+    return result
+
+def q21():
+    result = conn.query("""MATCH (f1:Films)<-[:A_JOUE]-(a:Actors)-[:A_JOUE]->(f2:Films)
+                        WHERE f1 <> f2
+                        WITH f1, COUNT(DISTINCT f2) AS NombreFilmsConnectes
+                        RETURN f1.title, NombreFilmsConnectes
+                        ORDER BY NombreFilmsConnectes DESC
+                        LIMIT 5;""") 
+    return result
+
+def q22():
+    result = conn.query("""MATCH (a:Actors)-[:A_JOUE]->(f:Films)<-[:A_REALISE]-(d:Realisateur)
+                        RETURN a.name, COUNT(DISTINCT d) AS NombreRealisateurs
+                        ORDER BY NombreRealisateurs DESC
+                        LIMIT 5;""") 
+    return result
+
+def q23():
+    result = conn.query("""MATCH (a:Actors {name: "Chris Pratt"})-[:A_JOUE]->(f:Films)
+                        UNWIND split(f.genre, ',') AS genre 
+                        WITH a, collect(DISTINCT genre) AS genres_pref
+                        MATCH (f2:Films)
+                        WHERE ANY(genre IN genres_pref WHERE genre IN f2.genre)
+                        AND NOT EXISTS { MATCH (a)-[:A_JOUE]->(f2) }  
+                        RETURN f2.title, f2.genre
+                        LIMIT 1;""") 
+    return result
+
+def q24_1():
+    result = conn.query("""MATCH ()-[r:INFLUENCE_PAR]->()
+                        RETURN count(r);""") 
+    return result
+
+def q24_2():
+    result = conn.query("""MATCH (d1:Realisateur)-[:INFLUENCE_PAR]->(d2:Realisateur {name: "Kenneth Lonergan"})
+                        RETURN d1.name;""") 
+    return result
+
+
+def q25():
+    result = conn.query("""MATCH p = shortestPath((a1:Actors {name: "Daniel Giménez Cacho"})-[*..10]-(a2:Actors {name: "Scarlett Johansson"}))
+                        RETURN p;""") 
+    return result
+
 
 # Fermer la connexion
 conn.close()
