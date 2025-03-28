0d34b43a931d02702c9487a2416fecb940f266ff
Sarah <riallandtardy@et.esiea.fr>
2025-03-28 02:36:26 +0100

    essaie partie transversale

--- connexion/transversale_queries.py
+++ connexion/transversale_queries.py
@@ -9,10 +9,10 @@ def q28(actor_name):
     MATCH (a:Actors {name: $actor_name})-[:A_JOUE]->(f:Films)
     UNWIND split(f.genre, ',') AS genre
     MATCH (f2:Films)
-    WHERE ANY(g IN split(f2.genre, ',') WHERE g = genre) AND NOT EXISTS {
-    MATCH (a)-[:A_JOUE]->(f2)}
-    RETURN f2.title AS RecommendedFilm, f2.genre AS Genre
+    WHERE ANY(g IN split(f2.genre, ',') WHERE g = genre) 
+      AND NOT EXISTS { MATCH (a)-[:A_JOUE]->(f2) }
+    RETURN DISTINCT f2.title AS RecommendedFilm, f2.genre AS Genre
     LIMIT 5
-    """, actor_name=actor_name)
-    return result
+    """, {"actor_name": actor_name})
+    return [f"{row['RecommendedFilm']} ({row['Genre']})" for row in result]
 
