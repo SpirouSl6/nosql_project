89cd3f42049fbe1b08252f4819cbf286e225cbac
Sarah <riallandtardy@et.esiea.fr>
2025-03-30 00:48:37 +0100

    essaie partie transversale

--- connexion/transversale_queries.py
+++ connexion/transversale_queries.py
@@ -4,6 +4,21 @@ from connexion.neo4j_co import Neo4jConnection
 conn = Neo4jConnection()
 
 
+def q27():
+    result = conn.query("""MATCH (f1:Films), (f2:Films)
+                        WHERE f1 <> f2 AND f1.title < f2.title  // Évite les doublons (A, B) et (B, A)
+                        UNWIND split(f1.genre, ',') AS genre1
+                        UNWIND split(f2.genre, ',') AS genre2
+                        WITH f1, f2, genre1, genre2
+                        WHERE genre1 = genre2
+                        WITH f1, f2, COLLECT(DISTINCT genre1) AS genres_communs
+                        RETURN f1.title AS Film1, f1.director AS Realisateur1, 
+                               f2.title AS Film2, f2.director AS Realisateur2, 
+                               genres_communs AS GenresPartages
+                        LIMIT 10;""") 
+    return result
+
+
 def q28(actor_name):
     result = conn.query("""
     MATCH (a:Actors {name: $actor_name})-[:A_JOUE]->(f:Films)
--- pages/transversale_page.py
+++ pages/transversale_page.py
@@ -2,8 +2,16 @@ import streamlit as st
 from connexion.transversale_queries import *
 
 
-#st.write(f"Films avec des genres en commun mais des réalisateurs différents : {q27()}")
-
+st.write(f"Films avec des genres en commun mais des réalisateurs différents :")
+for row in q27():
+    film1 = row['Film1']
+    realisateur1 = row['Realisateur1']
+    film2 = row['Film2']
+    realisateur2 = row['Realisateur2']
+    genres_communs = ", ".join(row['GenresPartages']) 
+    st.write(f"Films : {film1} ({realisateur1}) et {film2} ({realisateur2})")
+    st.write(f"Genres communs : {genres_communs}")
+    st.write("---")
 
 # Liste des acteurs
 acteurs = ["Chris Pratt", "Scarlett Johansson", "Robert Downey Jr.", "Ben Affleck", "Jane Levy", "Robert Pattinson"]
