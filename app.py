4673efd971746710bb3c8c9f0414c9b43e206aa7
Sarah <riallandtardy@et.esiea.fr>
2025-04-02 02:41:09 +0200

    teste sommaire

--- app.py
+++ app.py
@@ -4,20 +4,6 @@ st.title("NoSQL Databases - Projet")
 
 # Sommaire avec liens cliquables
 st.markdown("## Sommaire")
-st.markdown("""
-- [MongoDB](#mongodb)
-- [Neo4j](#neo4j)
-- [Transversale](#transversale)
-""", unsafe_allow_html=True)
-
-# Section MongoDB
-st.markdown("---")
-st.markdown("## - MongoDB", unsafe_allow_html=True)
-
-# Section Neo4j
-st.markdown("---")
-st.markdown("## - Neo4j", unsafe_allow_html=True)
-
-# Section Transversale
-st.markdown("---")
-st.markdown("## - Transversale", unsafe_allow_html=True)
+st.page_link("pages/mongo_page.py", label="MongoDB", icon="📂")
+st.page_link("pages/neo4j_page.py", label="Neo4j", icon="📂")
+st.page_link("pages/transversale_page.py", label="Transversale", icon="📂")
