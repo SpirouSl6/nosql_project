ec07fd7df5873c022c9c27f2c5c72e846821f6a0
Sarah <riallandtardy@et.esiea.fr>
2025-04-02 02:37:02 +0200

    teste sommaire

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
 st.title("NoSQL Databases - Projet")
+
+# Sommaire avec liens cliquables
+st.markdown("## Sommaire")
+st.markdown("""
+- [MongoDB](#mongodb)
+- [Neo4j](#neo4j)
+- [Transversale](#transversale)
+""", unsafe_allow_html=True)
+
+# Section MongoDB
+st.markdown("---")
+st.markdown("## - MongoDB", unsafe_allow_html=True)
+
+# Section Neo4j
+st.markdown("---")
+st.markdown("## - Neo4j", unsafe_allow_html=True)
+
+# Section Transversale
+st.markdown("---")
+st.markdown("## - Transversale", unsafe_allow_html=True)
