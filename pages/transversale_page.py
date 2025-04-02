ce3df4a701d2011a8a6348fbacc4651e157ca733
Sarah <riallandtardy@et.esiea.fr>
2025-04-02 02:31:45 +0200

    ajout des questions

--- pages/transversale_page.py
+++ pages/transversale_page.py
@@ -3,31 +3,31 @@ from connexion.transversale_queries import *
 
 st.title("NoSQL Databases - Projet")
 st.header("Questions transversales")
-st.write("")  # Ajoute un saut de ligne
-st.write("")
+st.markdown("")  # Ajoute un saut de ligne
+st.markdown("")
 
 st.markdown("#### **Q27. Quels sont les films qui ont des genres en commun mais qui ont des réalisateurs différents ?**")
-st.write(f"Les films avec des genres en commun mais des réalisateurs différents sont :")
+st.markdown(f"Les films avec des genres en commun mais des réalisateurs différents sont :")
 for row in q27():
     film1 = row['Film1']
     realisateur1 = row['Realisateur1']
     film2 = row['Film2']
     realisateur2 = row['Realisateur2']
     genres_communs = ", ".join(row['GenresPartages']) 
-    st.write(f"- Films : **{film1}** (de {realisateur1}) et **{film2}** (de {realisateur2}) avec **{genres_communs}** comme genre commun.")
-st.write("---")
+    st.markdown(f"- Films : **{film1}** (de {realisateur1}) et **{film2}** (de {realisateur2}) avec **{genres_communs}** comme genre commun.")
+st.markdown("---")
 
-st.markdown("#### **Q28. Recommander des films aux utilisateurs en fonction des préférences d’un acteur donné.**")
+st.markdown("#### **Q28. Recommander des films aux utilisateurs en fonction des préférences d'un acteur donné.**")
 # Liste des acteurs
 acteurs = ["Chris Pratt", "Scarlett Johansson", "Robert Downey Jr.", "Ben Affleck", "Jane Levy", "Robert Pattinson"]
 
 # Créer un menu déroulant (dropdown) pour choisir un acteur
 actor_name = st.selectbox("Choisissez un acteur que vous aimez", acteurs)
-st.write("Voici des films recommandés en fonction de l'acteur choisi :")
+st.markdown("Voici des films recommandés en fonction de l'acteur choisi :")
 st.markdown("\n".join(q28(actor_name)))
 
 st.markdown("#### **Q29. Créer une relation de “concurrence” entre réalisateurs ayant réalisé des films similaires la même année.**")
-st.write("Voici les relations de concurrence entre réalisateurs ayant réalisé des films similaires la même année :")
+st.markdown("Voici les relations de concurrence entre réalisateurs ayant réalisé des films similaires la même année :")
 # Affichage sous forme de liste
 for row in q29():
     realisateur1 = row['Realisateur1']
@@ -37,13 +37,13 @@ for row in q29():
     year = row['year']
     shared_genres = row['shared_genres']
 
-    st.write(f"- Concurrence entre **{realisateur1}** et **{realisateur2}** :")
-    st.write(f"**Films** : {film1} et {film2}, **Année** : {year}, **Genres partagés** : {', '.join(shared_genres)}")
-st.write("---")
+    st.markdown(f"- Concurrence entre **{realisateur1}** et **{realisateur2}** :")
+    st.markdown(f"**Films** : {film1} et {film2}, **Année** : {year}, **Genres partagés** : {', '.join(shared_genres)}\n")
+st.markdown("---")
 
 
 st.markdown("#### **Q30. Identifier les collaborations les plus fréquentes entre réalisateurs et acteurs, puis analyser si ces collaborations sont associées à un succès commercial ou critique.**")
-st.write("Voici les collaborations les plus fréquentes entre réalisateurs et acteurs avec leurs succès commercial et critique :")
+st.markdown("Voici les collaborations les plus fréquentes entre réalisateurs et acteurs avec leurs succès commercial et critique :")
 
 # Affichage sous forme de liste
 for row in q30():
@@ -53,9 +53,9 @@ for row in q30():
     ratings = row['ratings']
     total_revenue = row['total_revenue']
     
-    st.write(f"- Collaboration entre le réalisateur **{realisateur}** et l'acteur **{acteur}** :")
-    st.write(f"**Nombre de collaborations** : {collaboration_count}, **Liste des notes des films** : {ratings}, **Revenu total des films** : {total_revenue:,.2f}€")
-st.write("---")
+    st.markdown(f"- Collaboration entre le réalisateur **{realisateur}** et l'acteur **{acteur}** :")
+    st.markdown(f"**Nombre de collaborations** : {collaboration_count}, **Liste des notes des films** : {ratings}, **Revenu total des films** : {total_revenue:,.2f}€\n")
+st.markdown("---")
 
 
 
